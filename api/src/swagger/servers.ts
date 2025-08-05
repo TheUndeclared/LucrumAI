7e23d33210c93e11c0f016e9b02de4f8ee13f965
Hamza Awan <mh89371@gmail.com>
2025-08-05 23:19:12 +0500

    Inital Backend and CircleCI Pipeline

---
+++ api/src/swagger/servers.ts
@@ -0,0 +1,20 @@
+export default {
+  servers: [
+    {
+      url: 'https://api-dev.soltradeai.xyz',
+      description: 'Development',
+    },
+    {
+      url: 'https://api-staging.soltradeai.xyz',
+      description: 'Staging',
+    },
+    {
+      url: 'https://api.soltradeai.xyz',
+      description: 'Staging',
+    },
+    {
+      url: 'http://localhost:5000',
+      description: 'Local',
+    },
+  ],
+};
